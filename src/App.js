04ed45066bc76c0380fd1277cb9e80d6086cee1d
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-12-26 03:16:02 +0200

    add border radius change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useCallback } from 'react';
-import axios from 'axios';
 import { useDropzone } from 'react-dropzone';
-import './App.css'
+import html2canvas from 'html2canvas';
+import './App.css';
 
 const App = () => {
   const [selectedImage, setSelectedImage] = useState(null);
@@ -11,17 +11,21 @@ const App = () => {
   const [sepia, setSepia] = useState(0);
   const [grayscale, setGrayscale] = useState(0);
   const [blur, setBlur] = useState(0);
+  const [hue, setHue] = useState(0);
+  const [saturation, setSaturation] = useState(100);
+  const [inversion, setInversion] = useState(0);
+  const [borderRadius, setBorderRadius] = useState(0);
   const [downloadPressed, setDownloadPressed] = useState(false);
 
   const onDrop = useCallback((acceptedFiles) => {
     setSelectedImage(acceptedFiles[0]);
   }, []);
 
-  // const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
     noClick: true, // Disable click-to-select
   });
+
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     setSelectedImage(file);
@@ -41,7 +45,8 @@ const App = () => {
         canvas.height = img.height;
 
         // Apply filters
-        ctx.filter = `brightness(${brightness}%) contrast(${contrast}%) sepia(${sepia}%) grayscale(${grayscale}%) blur(${blur}px)`;
+        ctx.filter = `brightness(${brightness}%) contrast(${contrast}%) sepia(${sepia}%) grayscale(${grayscale}%) blur(${blur}px) hue-rotate(${hue}deg) saturate(${saturation}%) invert(${inversion}%)`;
+        ctx.canvas.style.borderRadius = `${borderRadius}%`;
 
         // Draw the image on the canvas
         ctx.drawImage(img, 0, 0);
@@ -55,131 +60,217 @@ const App = () => {
     });
   };
 
-
   const downloadImage = async () => {
-    await applyImageEffects(); // Apply effects before downloading
-    setDownloadPressed(true);
-  };
+    // Apply effects before downloading
+    await applyImageEffects();
 
-  const handleImageLoad = () => {
-    if (downloadPressed) {
+    // Check if the border radius is changed
+    if (borderRadius !== 0) {
+      // Use HTML2Canvas to capture the modified image
+      html2canvas(document.getElementById('image-container')).then((canvas) => {
+        const dataUrl = canvas.toDataURL('image/jpeg');
+
+        // Trigger the download
+        const link = document.createElement('a');
+        link.href = dataUrl;
+        link.download = 'modified_image.png';
+        link.click();
+      });
+    } else {
+      // If border radius is not changed, use the original resultImage for download
       const link = document.createElement('a');
       link.href = resultImage;
       link.download = 'modified_image.png';
       link.click();
+    }
+
+    // Reset the downloadPressed state
+    setDownloadPressed(false);
+  };
+
+  const handleImageLoad = () => {
+    if (downloadPressed) {
+      // Check if the border radius is changed
+      if (borderRadius !== 0) {
+        // Use HTML2Canvas to capture the modified image
+        html2canvas(document.getElementById('image-container')).then((canvas) => {
+          const dataUrl = canvas.toDataURL('image/jpeg');
+
+          // Trigger the download
+          const link = document.createElement('a');
+          link.href = dataUrl;
+          link.download = 'modified_image.png';
+          link.click();
+        });
+      } else {
+        // If border radius is not changed, use the original resultImage for download
+        const link = document.createElement('a');
+        link.href = resultImage;
+        link.download = 'modified_image.png';
+        link.click();
+      }
+
+      // Reset the downloadPressed state
       setDownloadPressed(false);
     }
   };
 
   return (
     <div>
-    
-    {!selectedImage ? (
-      <div
-      {...getRootProps()}
-      style={{
-        width: '100vw',
-        height: '100vh',
-        background: `${isDragActive ? 'yellow' : 'transparent'}`,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-       
-      }}
-      >
-      <input
-    type="file"
-    id="file-input"
-     name="file-input"
-    onChange={handleImageChange}
-  />
-  <div className="drop-or-select">
-  <label id="file-input-label" for="file-input">Upload Image</label>
-  <p>or drop a file</p>
-  </div>
+      {!selectedImage ? (
+        <div
+          className={!isDragActive ? 'drop-zone' : 'drop-zone active-drop-zone'}
+          {...getRootProps()}
+        >
+          <input
+            type="file"
+            id="file-input"
+            name="file-input"
+            onChange={handleImageChange}
+          />
+          <div className="drop-or-select">
+            {!isDragActive ? (
+              <div>
+                <label id="file-input-label" htmlFor="file-input">
+                  Upload Image
+                </label>
+                <p>or drop a file</p>
+              </div>
+            ) : (
+              <h1>Drop image anywhere</h1>
+            )}
+          </div>
           <input {...getInputProps()} />
         </div>
       ) : (
-        <div>
-          <img
-            src={URL.createObjectURL(selectedImage)}
-            alt="Selected Image"
-            style={{
-              width: '700px',
-              height: '700px',
-              margin: 'auto',
-              marginBottom: '20px',
-              filter: `brightness(${brightness}%) contrast(${contrast}%) sepia(${sepia}%) grayscale(${grayscale}%) blur(${blur}px)`,
-            }}
-            onLoad={handleImageLoad}
-          />
-          <div>
-            <label htmlFor="brightness">Brightness:</label>
-            <input
-              type="range"
-              id="brightness"
-              name="brightness"
-              min="0"
-              max="200"
-              value={brightness}
-              onChange={(e) => setBrightness(e.target.value)}
+        <div className="edit">
+          <div className="sliders">
+            <div className="slider">
+              <label htmlFor="brightness">Brightness:</label>
+              <input
+                type="range"
+                id="brightness"
+                name="brightness"
+                min="0"
+                max="200"
+                value={brightness}
+                onChange={(e) => setBrightness(e.target.value)}
+              />
+            </div>
+            <div className="slider">
+              <label htmlFor="contrast">Contrast:</label>
+              <input
+                type="range"
+                id="contrast"
+                name="contrast"
+                min="0"
+                max="200"
+                value={contrast}
+                onChange={(e) => setContrast(e.target.value)}
+              />
+            </div>
+            <div className="slider">
+              <label htmlFor="sepia">Sepia:</label>
+              <input
+                type="range"
+                id="sepia"
+                name="sepia"
+                min="0"
+                max="100"
+                value={sepia}
+                onChange={(e) => setSepia(e.target.value)}
+              />
+            </div>
+            <div className="slider">
+              <label htmlFor="grayscale">Grayscale:</label>
+              <input
+                type="range"
+                id="grayscale"
+                name="grayscale"
+                min="0"
+                max="100"
+                value={grayscale}
+                onChange={(e) => setGrayscale(e.target.value)}
+              />
+            </div>
+            <div className="slider">
+              <label htmlFor="blur">Blur:</label>
+              <input
+                type="range"
+                id="blur"
+                name="blur"
+                min="0"
+                max="10"
+                step="0.1"
+                value={blur}
+                onChange={(e) => setBlur(e.target.value)}
+              />
+            </div>
+            <div className="slider">
+              <label htmlFor="hue">Hue:</label>
+              <input
+                type="range"
+                id="hue"
+                name="hue"
+                min="0"
+                max="360"
+                value={hue}
+                onChange={(e) => setHue(e.target.value)}
+              />
+            </div>
+            <div className="slider">
+              <label htmlFor="saturation">Saturation:</label>
+              <input
+                type="range"
+                id="saturation"
+                name="saturation"
+                min="0"
+                max="200"
+                value={saturation}
+                onChange={(e) => setSaturation(e.target.value)}
+              />
+            </div>
+            <div className="slider">
+              <label htmlFor="inversion">Inversion:</label>
+              <input
+                type="range"
+                id="inversion"
+                name="inversion"
+                min="0"
+                max="100"
+                value={inversion}
+                onChange={(e) => setInversion(e.target.value)}
+              />
+            </div>
+            <div className="slider">
+              <label htmlFor="borderRadius">Border Radius:</label>
+              <input
+                type="range"
+                id="borderRadius"
+                name="borderRadius"
+                min="0"
+                max="50"
+                value={borderRadius}
+                onChange={(e) => setBorderRadius(e.target.value)}
+              />
+            </div>
+            <button onClick={downloadImage}>Download Image</button>
+          </div>
+          <div id="image-container">
+            <img
+              src={URL.createObjectURL(selectedImage)}
+              alt="Selected Image"
+              style={{
+                filter: `brightness(${brightness}%) contrast(${contrast}%) sepia(${sepia}%) grayscale(${grayscale}%) blur(${blur}px) hue-rotate(${hue}deg) saturate(${saturation}%) invert(${inversion}%)`,
+                borderRadius: `${borderRadius}%`,
+              }}
+              onLoad={handleImageLoad}
             />
           </div>
-          <div>
-            <label htmlFor="contrast">Contrast:</label>
-            <input
-              type="range"
-              id="contrast"
-              name="contrast"
-              min="0"
-              max="200"
-              value={contrast}
-              onChange={(e) => setContrast(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="sepia">Sepia:</label>
-            <input
-              type="range"
-              id="sepia"
-              name="sepia"
-              min="0"
-              max="100"
-              value={sepia}
-              onChange={(e) => setSepia(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="grayscale">Grayscale:</label>
-            <input
-              type="range"
-              id="grayscale"
-              name="grayscale"
-              min="0"
-              max="100"
-              value={grayscale}
-              onChange={(e) => setGrayscale(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="blur">Blur:</label>
-            <input
-              type="range"
-              id="blur"
-              name="blur"
-              min="0"
-              max="10"
-              step="0.1"
-              value={blur}
-              onChange={(e) => setBlur(e.target.value)}
-            />
-          </div>
-          <button onClick={downloadImage}>Download Image</button>
-          {resultImage && <img src={resultImage} alt="Result" />}
         </div>
       )}
     </div>
   );
 };
 
-export default App
+export default App;
