5849981856c2c5229c7928dd05f8ad96fd5ad295
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-12-27 05:53:46 +0200

    edit background

--- src/ImageEditor.jsx
+++ src/ImageEditor.jsx
@@ -1,18 +1,17 @@
 import React, { useState, useRef, useCallback } from 'react';
 import html2canvas from 'html2canvas';
 import { useDropzone } from 'react-dropzone';
-import { toPng } from "html-to-image";
-import reflectHorizontal from './reflect horizontal.png'
-import reflectVertical from './reflect vertical.png'
+import { toPng } from 'html-to-image';
+import reflectHorizontal from './reflect horizontal.png';
+import reflectVertical from './reflect vertical.png';
 import HomeIcon from '@mui/icons-material/Home';
 import Button from '@mui/material/Button';
 import FlipIcon from '@mui/icons-material/Flip';
-import MenuIcon from "@mui/icons-material/Menu";
+import MenuIcon from '@mui/icons-material/Menu';
 import RotateRightIcon from '@mui/icons-material/RotateRight';
 import RotateLeftIcon from '@mui/icons-material/RotateLeft';
 import DownloadingIcon from '@mui/icons-material/Downloading';
-import bgImg from './ed-it bg.jpg'
-
+import bgImg from './ed-it bg.jpg';
 
 import './ImageEditor.css';
 
@@ -50,8 +49,8 @@ const ImageEditor = () => {
   const htmlToImageConvert = () => {
     toPng(imageRef.current, { cacheBust: false })
       .then((dataUrl) => {
-        const link = document.createElement("a");
-        link.download = "my-image-name.png";
+        const link = document.createElement('a');
+        link.download = 'my-image-name.png';
         link.href = dataUrl;
         link.click();
       })
@@ -89,16 +88,12 @@ const ImageEditor = () => {
     setRotate((rotate) => (rotate + 90) % 360);
   };
   const flipHorizontal = () => {
-    if (flipX===1)
-    setFlipX(-1);
-  else
-  setFlipX(1);
+    if (flipX === 1) setFlipX(-1);
+    else setFlipX(1);
   };
   const flipVertical = () => {
-    if (flipY===1)
-    setFlipY(-1);
-  else
-  setFlipY(1);
+    if (flipY === 1) setFlipY(-1);
+    else setFlipY(1);
   };
 
   const handleBorderRadiusChange = (e) => {
@@ -106,23 +101,23 @@ const ImageEditor = () => {
   };
 
   const handleDownload = () => {
-      html2canvas(imageRef.current, { backgroundColor: null }).then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
-        const link = document.createElement('a');
-        link.href = imgData;
-        link.download = 'edited_image.png';
-        link.click();
-      });
-    };
+    html2canvas(imageRef.current, { backgroundColor: null }).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const link = document.createElement('a');
+      link.href = imgData;
+      link.download = 'edited_image.png';
+      link.click();
+    });
+  };
 
   return (
     <div>
-    {!selectedImage ? (
-      <div
-      className={!isDragActive ? 'drop-zone' : 'drop-zone active-drop-zone'}
-      {...getRootProps()}
-      >
-      <h1>Ed-it!</h1>
+      {!selectedImage ? (
+        <div
+          className='drop-zone'
+          {...getRootProps()}
+        >
+          <h1>Ed-it!</h1>
           <input
             type='file'
             id='file-input'
@@ -130,166 +125,182 @@ const ImageEditor = () => {
             onChange={handleImageChange}
           />
           <div className='drop-or-select'>
-            {!isDragActive ? (
-              <div>
-                <label id='file-input-label' htmlFor='file-input'>
-                  Upload Image
-                </label>
-                <p>or drop a file</p>
+            <div>
+              <label id='file-input-label' htmlFor='file-input'>
+                Upload Image
+              </label>
+              <p>or drop a file</p>
+            </div>
+          </div>
+          {isDragActive && (
+            <div>
+              <input {...getInputProps()} />
+              <div className='layer'>
+                <h1>Drop image anywhere</h1>
               </div>
-            ) : (
-              <h1>Drop image anywhere</h1>
-            )}
-          </div>
-          <input {...getInputProps()} />
-          <div className='layer'>
-          <h1>Drop image anywhere</h1>
-          </div>
+            </div>
+          )}
         </div>
       ) : (
         <div className='edit'>
           <div className='sliders'>
-          <div className="slider">
-          <label htmlFor="brightness">Brightness:</label>
-          <input
-            type="range"
-            id="brightness"
-            name="brightness"
-            min="0"
-            max="200"
-            value={brightness}
-            onChange={(e) => setBrightness(e.target.value)}
-          />
-        </div>
-        <div className="slider">
-          <label htmlFor="contrast">Contrast:</label>
-          <input
-            type="range"
-            id="contrast"
-            name="contrast"
-            min="0"
-            max="200"
-            value={contrast}
-            onChange={(e) => setContrast(e.target.value)}
-          />
-        </div>
-        <div className="slider">
-          <label htmlFor="sepia">Sepia:</label>
-          <input
-            type="range"
-            id="sepia"
-            name="sepia"
-            min="0"
-            max="100"
-            value={sepia}
-            onChange={(e) => setSepia(e.target.value)}
-          />
-        </div>
-        <div className="slider">
-          <label htmlFor="grayscale">Grayscale:</label>
-          <input
-            type="range"
-            id="grayscale"
-            name="grayscale"
-            min="0"
-            max="100"
-            value={grayscale}
-            onChange={(e) => setGrayscale(e.target.value)}
-          />
-        </div>
-        <div className="slider">
-          <label htmlFor="blur">Blur:</label>
-          <input
-            type="range"
-            id="blur"
-            name="blur"
-            min="0"
-            max="10"
-            step="0.1"
-            value={blur}
-            onChange={(e) => setBlur(e.target.value)}
-          />
-        </div>
-        <div className="slider">
-          <label htmlFor="hue">Hue:</label>
-          <input
-            type="range"
-            id="hue"
-            name="hue"
-            min="0"
-            max="360"
-            value={hue}
-            onChange={(e) => setHue(e.target.value)}
-          />
-        </div>
-        <div className="slider">
-          <label htmlFor="saturation">Saturation:</label>
-          <input
-            type="range"
-            id="saturation"
-            name="saturation"
-            min="0"
-            max="200"
-            value={saturation}
-            onChange={(e) => setSaturation(e.target.value)}
-          />
-        </div>
-        <div className="slider">
-          <label htmlFor="inversion">Inversion:</label>
-          <input
-            type="range"
-            id="inversion"
-            name="inversion"
-            min="0"
-            max="100"
-            value={inversion}
-            onChange={(e) => setInversion(e.target.value)}
-          />
-        </div>
-
-        <div className="slider">
-        <label>Border Radius:</label>
-        <input
-          type='range'
-          min='0'
-          max='50'
-          value={borderRadius}
-          onChange={(e) => setBorderRadius(e.target.value)}
-          />
-        </div>
-        <Button onClick={flipHorizontal} startIcon={<FlipIcon className='icon horizontal' />}></Button>
-        <Button onClick={flipVertical} startIcon={<FlipIcon className='icon vertical' />}></Button>
-        <Button onClick={rotateRight} startIcon={<RotateRightIcon className='icon' />}></Button>
-        <Button onClick={rotateLeft} startIcon={<RotateLeftIcon className='icon' />}></Button>
-       
-          <Button onClick={htmlToImageConvert} startIcon={<DownloadingIcon  />}>Download Image</Button>
-          </div>
-
-            <div>
-
-              <div
-                ref={imageRef}
-                style={{
-                  borderRadius: `${borderRadius}%`,
-                  transform: `rotate(${rotate}deg) scaleX(${flipX}) scaleY(${flipY})`,
-                 
-                  // transform: 'perspective(230px) rotateY(-8deg)',
-                  filter: `brightness(${brightness}%) contrast(${contrast}%) sepia(${sepia}%) grayscale(${grayscale}%) blur(${blur}px) hue-rotate(${hue}deg) saturate(${saturation}%) invert(${inversion}%)`,
-                  // transformOrigin: '50% 50%', // Set the rotation point to the center of the image
-
-
-                  overflow: 'hidden',
-
-                }}
-              >
-              <img id='image' src={URL.createObjectURL(selectedImage)} alt='Edited' />
-              </div>
+            <div className='slider'>
+              <label htmlFor='brightness'>Brightness:</label>
+              <input
+                type='range'
+                id='brightness'
+                name='brightness'
+                min='0'
+                max='200'
+                value={brightness}
+                onChange={(e) => setBrightness(e.target.value)}
+              />
+            </div>
+            <div className='slider'>
+              <label htmlFor='contrast'>Contrast:</label>
+              <input
+                type='range'
+                id='contrast'
+                name='contrast'
+                min='0'
+                max='200'
+                value={contrast}
+                onChange={(e) => setContrast(e.target.value)}
+              />
+            </div>
+            <div className='slider'>
+              <label htmlFor='sepia'>Sepia:</label>
+              <input
+                type='range'
+                id='sepia'
+                name='sepia'
+                min='0'
+                max='100'
+                value={sepia}
+                onChange={(e) => setSepia(e.target.value)}
+              />
+            </div>
+            <div className='slider'>
+              <label htmlFor='grayscale'>Grayscale:</label>
+              <input
+                type='range'
+                id='grayscale'
+                name='grayscale'
+                min='0'
+                max='100'
+                value={grayscale}
+                onChange={(e) => setGrayscale(e.target.value)}
+              />
+            </div>
+            <div className='slider'>
+              <label htmlFor='blur'>Blur:</label>
+              <input
+                type='range'
+                id='blur'
+                name='blur'
+                min='0'
+                max='10'
+                step='0.1'
+                value={blur}
+                onChange={(e) => setBlur(e.target.value)}
+              />
+            </div>
+            <div className='slider'>
+              <label htmlFor='hue'>Hue:</label>
+              <input
+                type='range'
+                id='hue'
+                name='hue'
+                min='0'
+                max='360'
+                value={hue}
+                onChange={(e) => setHue(e.target.value)}
+              />
+            </div>
+            <div className='slider'>
+              <label htmlFor='saturation'>Saturation:</label>
+              <input
+                type='range'
+                id='saturation'
+                name='saturation'
+                min='0'
+                max='200'
+                value={saturation}
+                onChange={(e) => setSaturation(e.target.value)}
+              />
+            </div>
+            <div className='slider'>
+              <label htmlFor='inversion'>Inversion:</label>
+              <input
+                type='range'
+                id='inversion'
+                name='inversion'
+                min='0'
+                max='100'
+                value={inversion}
+                onChange={(e) => setInversion(e.target.value)}
+              />
             </div>
 
+            <div className='slider'>
+              <label>Border Radius:</label>
+              <input
+                type='range'
+                min='0'
+                max='50'
+                value={borderRadius}
+                onChange={(e) => setBorderRadius(e.target.value)}
+              />
+            </div>
+            <Button
+              onClick={flipHorizontal}
+              startIcon={<FlipIcon className='icon horizontal' />}
+            ></Button>
+            <Button
+              onClick={flipVertical}
+              startIcon={<FlipIcon className='icon vertical' />}
+            ></Button>
+            <Button
+              onClick={rotateRight}
+              startIcon={<RotateRightIcon className='icon' />}
+            ></Button>
+            <Button
+              onClick={rotateLeft}
+              startIcon={<RotateLeftIcon className='icon' />}
+            ></Button>
+
+            <Button
+              onClick={htmlToImageConvert}
+              startIcon={<DownloadingIcon />}
+            >
+              Download Image
+            </Button>
+          </div>
+
+          <div>
+            <div
+              ref={imageRef}
+              style={{
+                borderRadius: `${borderRadius}%`,
+                transform: `rotate(${rotate}deg) scaleX(${flipX}) scaleY(${flipY})`,
+
+                // transform: 'perspective(230px) rotateY(-8deg)',
+                filter: `brightness(${brightness}%) contrast(${contrast}%) sepia(${sepia}%) grayscale(${grayscale}%) blur(${blur}px) hue-rotate(${hue}deg) saturate(${saturation}%) invert(${inversion}%)`,
+                // transformOrigin: '50% 50%', // Set the rotation point to the center of the image
+
+                overflow: 'hidden',
+              }}
+            >
+              <img
+                id='image'
+                src={URL.createObjectURL(selectedImage)}
+                alt='Edited'
+              />
+            </div>
+          </div>
         </div>
       )}
     </div>
   );
 };
 export default ImageEditor;
-
