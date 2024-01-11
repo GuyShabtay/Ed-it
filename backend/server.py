6496033c8f224cadce77c11d3fa3f608e8986df5
Guy Shabtay <guysh16@ac.sce.ac.il>
2024-01-11 20:01:55 +0200

    change port to 10000

--- backend/server.py
+++ backend/server.py
@@ -34,5 +34,7 @@ def remove_background():
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    # Use the PORT environment variable, default to 10000
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
 
