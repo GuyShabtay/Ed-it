d165f1ecbb1ff7124854b00c2ae71a08c16eba3b
Guy Shabtay <guysh16@ac.sce.ac.il>
2024-01-18 17:32:10 +0200

    call to server online

--- frontend/src/ImageEditor.jsx
+++ frontend/src/ImageEditor.jsx
@@ -67,7 +67,7 @@ const ImageEditor = () => {
       if (imageRef.current) {
         const dataUrl = await toPng(imageRef.current, { cacheBust: false });
         const response = await axios.post(
-          'http://127.0.0.1:5000/api/remove-background',
+          'https://ed-it-backend.onrender.com:5000/api/remove-background',
           {
             imageData: dataUrl.split(',')[1],
           }
