f01c8e479ab045c10f11d1658e8b64c3d6c49f2c
Guy Shabtay <guysh16@ac.sce.ac.il>
2024-01-25 00:19:10 +0200

    update timeout

--- backend/server.py
+++ backend/server.py
@@ -38,4 +38,5 @@ def remove_background():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, timeout=300)  # Set timeout to 300 seconds (adjust as needed)
+
