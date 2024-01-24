5d8c7dc067f56107b718a48f292626b9ea11e809
Guy Shabtay <guysh16@ac.sce.ac.il>
2024-01-25 01:35:24 +0200

    change to fast api

--- backend/server.py
+++ backend/server.py
@@ -1,29 +1,33 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+from fastapi import FastAPI, HTTPException, UploadFile, Form
+from fastapi.middleware.cors import CORSMiddleware
 from rembg import remove
 from PIL import Image
 import base64
 import io
 
-app = Flask(__name__)
-CORS(app)
-app.config['TIMEOUT'] = 300
+app = FastAPI()
+
+# Enable CORS (Cross-Origin Resource Sharing) for all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def run():
-    return "Server Running"
+    return {"message": "Server Running"}
 
-@app.route('/api/remove-background', methods=['POST'])
-def remove_background():
+@app.post("/api/remove-background")
+async def remove_background(image: UploadFile = Form(...)):
     try:
-        # Get the image data from the request
-        image_data = request.json.get('imageData')
-        print("hi")
-        # Decode the base64 image data
-        image_bytes = base64.b64decode(image_data)
+        # Read image file
+        image_data = await image.read()
 
         # Remove background
-        input_image = Image.open(io.BytesIO(image_bytes))
+        input_image = Image.open(io.BytesIO(image_data))
         output_image = remove(input_image)
 
         # Convert output image to base64
@@ -31,13 +35,14 @@ def remove_background():
         output_image.save(buffered, format="PNG")
         output_image_data = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
-        return jsonify({'outputImageData': output_image_data})
+        return {"outputImageData": output_image_data}
 
     except Exception as e:
         print(str(e))  # Print the error for debugging
-        return jsonify({'error': str(e)}), 500
-
+        raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Set timeout to 300 seconds (adjust as needed)
+    import uvicorn
 
+    # Run the application using Uvicorn server
+    uvicorn.run(app, host='0.0.0.0', port=5000)
